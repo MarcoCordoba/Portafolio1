6d92ede50cda72f5110809b61a29e1880e57ee72
Marco Córdoba <marco.cba2@gmail.com>
2025-04-01 16:03:37 -0300

    inclusion de iconos en el home y cambios en el footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Perfil from '../assets/images/Perfil.jpg';
 import './Home.css';
+import LinkedIn from "./icons/LinkedIn";
+import Github from "./icons/Github";
 
 const Home = () => {
     return(
@@ -8,6 +10,13 @@ const Home = () => {
             <div className="home-texto">
                 <h1>Marco Cordoba</h1>
                 <h2>Full-Stack Developer</h2>
+                <a href="https://www.linkedin.com/in/marco-cordoba-/" target="_blank" rel="noopener noreferrer">
+                    <LinkedIn className="icono" />
+                </a>
+                <a href="https://github.com/MarcoCordoba" target="_blank" rel="noopener noreferrer">
+                    <Github className="icono" style={{ fill: "black" }} />
+                </a>
+
             </div>
             <div className="home-imagen">
                 <img src={Perfil} alt="marco cordoba" />
