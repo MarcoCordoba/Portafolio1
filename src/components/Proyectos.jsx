d7a9c8f614cac67f9153fd8bfc8e776a4294b682
Marco Córdoba <marco.cba2@gmail.com>
2025-04-02 12:48:18 -0300

    cambios en las cards de proyectos y colores generales

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,24 +1,42 @@
 import React from "react";
 import "./proyectos.css";
 
+// Importando iconos desde la carpeta "icons"
+import Css from "./icons/Css"
+import Html5 from "./icons/Html5"
+import Python from "./icons/Python"
+import Django from "./icons/Django"
+import PostgreSQL from "./icons/PostgreSQL"
+import ReactI from "./icons/ReactI"
+import Javascript from "./icons/Javascript"
+import GitHub from "./icons/Github"
+
+//Importando imagenes representativas de los proyectos
+import notetask from "../assets/images/notetask.jpg";
+import Negro from "../assets/Images/Negro.jpg"
+
+
 const proyectos = [
   {
     titulo: "Gestor de Reservas",
-    descripcion: "Aplicación web para gestionar alquileres temporales con Django y PostgreSQL.",
-    imagen: "/assets/gestor-reservas.png", // Colocá la imagen en assets
-    github: "https://github.com/tuusuario/gestor-reservas",
+    descripcion: "Aplicación web para gestionar alquileres temporales",
+    imagen: notetask,
+    tecnologias: [<Django />,<Python/>, <PostgreSQL />, <Html5 />, <Css />],
+    github: "",
   },
   {
     titulo: "Portafolio Web",
     descripcion: "Mi portafolio personal hecho con React y CSS.",
-    imagen: "/assets/portafolio.png",
-    github: "https://github.com/tuusuario/portafolio-web",
+    imagen: Negro,
+    tecnologias: [<ReactI />, <Css />, <Html5 />, <Javascript />],
+    github: "",
   },
   {
-    titulo: "App de gestion de tareas",
-    descripcion: "App con login para administrar las tareas de cada usuario. Creada con django, postgresql, html, css y javascript ",
-    imagen: "/assets/ecommerce.png",
-    github: "https://github.com/MarcoCordoba/app_notas.git",
+    titulo: "Gestor de Tareas",
+    descripcion: "Aplicacion para administrar las tareas de un usuario registrado y logeado.",
+    imagen: Negro,
+    tecnologias: [<Django />, <PostgreSQL />, <Html5 />, <Css />, <Javascript />, <Python />],
+    github: "",
   },
 ];
 
@@ -30,11 +48,24 @@ const Proyectos = () => {
         {proyectos.map((proyecto, index) => (
           <div className="proyecto-card" key={index}>
             <img src={proyecto.imagen} alt={proyecto.titulo} className="proyecto-img" />
-            <h3>{proyecto.titulo}</h3>
-            <p>{proyecto.descripcion}</p>
-            <a href={proyecto.github} target="_blank" rel="noopener noreferrer" className="github-link">
-              Ver código en GitHub
-            </a>
+            <div className="proyecto-content">
+              <h3 className="proyecto-title">{proyecto.titulo}</h3>
+              <p className="proyecto-desc">{proyecto.descripcion}</p>
+              <div className="proyecto-tech">
+                {proyecto.tecnologias.map((icono, i) => (
+                  <span className="tech-icon" key={i}>{icono}</span>
+                ))}
+              </div>
+              <div className="proyecto-links">
+                <a href={proyecto.github} target="_blank" rel="noopener noreferrer" className="proyecto-icon">
+                  <GitHub />
+                </a>
+                {proyecto.link && (
+                  <a href={proyecto.link} target="_blank" rel="noopener noreferrer" className="proyecto-icon">
+                  </a>
+                )}
+              </div>
+            </div>
           </div>
         ))}
       </div>
