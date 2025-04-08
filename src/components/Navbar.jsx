18f6c1bfc8416d657e8ef7964524b92c18697f8b
Marco Córdoba <marco.cba2@gmail.com>
2025-04-08 18:46:16 -0300

    modificacion en los links de las secciones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return(
         <nav className="navbar">
             <a href="#home" className="nombre_nav">Marco Cordoba Dev</a>
-            <ul>
+            <ul className="nav-links">
                 <li><a href="#Sobre-mi" className="a">Sobre mi</a></li>
                 <li><a href="#Proyectos" className="a">Proyectos</a></li>
                 <li><a href="#Tecnologias" className="a">Tecnologias</a></li>
